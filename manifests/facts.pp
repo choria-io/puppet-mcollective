f5be2f308d2af67ea38cf1c8438caa10dd73c0d4
tek0011 <thecptsparrow@gmail.com>
2017-10-31 13:15:01 -0500

    add cron_offset to allow ensure => absent to work
    Fix for #143

--- manifests/facts.pp
+++ manifests/facts.pp
@@ -1,17 +1,17 @@
 class mcollective::facts (
-  String $libdir = $mcollective::libdir,
-  String $rubypath = $mcollective::rubypath,
-  String $configdir = $mcollective::configdir,
-  String $factspath = $mcollective::factspath,
+  String $libdir            = $mcollective::libdir,
+  String $rubypath          = $mcollective::rubypath,
+  String $configdir         = $mcollective::configdir,
+  String $factspath         = $mcollective::factspath,
   Integer $refresh_interval = $mcollective::facts_refresh_interval,
-  Optional[String] $owner = $mcollective::plugin_owner,
-  Optional[String] $group = $mcollective::plugin_group,
+  Optional[String] $owner   = $mcollective::plugin_owner,
+  Optional[String] $group   = $mcollective::plugin_group,
   Optional[String] $pidfile = $mcollective::facts_pidfile,
-  Boolean $server = $mcollective::server
+  Boolean $server           = $mcollective::server
 ) {
   $scriptpath = "${libdir}/mcollective/refresh_facts.rb"
 
-  file{$scriptpath:
+  file { $scriptpath:
     owner   => $owner,
     group   => $group,
     mode    => "0755",
@@ -20,14 +20,16 @@ class mcollective::facts (
 
   if $refresh_interval > 0 and $server {
     $cron_ensure = "present"
+    $cron_offset = "00:${sprintf("%02d", fqdn_rand($refresh_interval, 'facts cronjob'))}"
     $creates = $factspath
   } else {
     $cron_ensure = "absent"
+    $cron_offset = "00:00"
     $creates = undef # always run via puppet when opted out of cron option
   }
 
   if $server {
-    exec{"mcollective_facts_yaml_refresh":
+    exec { "mcollective_facts_yaml_refresh":
       command => "\"${rubypath}\" \"${scriptpath}\" -o \"${factspath}\"",
       creates => $creates,
       require => Class["mcollective::service"]
@@ -40,18 +42,18 @@ class mcollective::facts (
 
   if $facts["os"]["family"] == "windows" {
     # on windows task scheduler prevent dupes already so no need to handle the PID here
-    scheduled_task{"mcollective_facts_yaml_refresh":
+    scheduled_task { "mcollective_facts_yaml_refresh":
       ensure    => $cron_ensure,
       command   => $rubypath,
       arguments => "'${scriptpath}' -o '${factspath}'",
       trigger   => {
         "schedule"         => "daily",
-        "start_time"       => "00:${sprintf("%02d", fqdn_rand($refresh_interval, 'facts cronjob'))}",
+        "start_time"       => $cron_offset,
         "minutes_interval" => $refresh_interval
       }
     }
   } else {
-    cron{"mcollective_facts_yaml_refresh":
+    cron { "mcollective_facts_yaml_refresh":
       ensure  => $cron_ensure,
       command => "'${rubypath}' '${scriptpath}' -o '${factspath}' ${factspid}",
       minute  => mcollective::crontimes(fqdn_rand($refresh_interval, 'facts cronjob'), $refresh_interval, 60)
