f1796ccf93b1c23350e72db7a414a560af753b04
R.I.Pienaar <rip@devco.net>
2018-11-03 17:04:17 +0100

    (#200) create old mcollective dirs on AIO 6
    When running on AIO 6 create the old mcollective directories

--- manifests/plugin_dirs.pp
+++ manifests/plugin_dirs.pp
@@ -3,13 +3,22 @@ class mcollective::plugin_dirs {
     "${mcollective::libdir}/mcollective/${type}"
   }
 
+  if "aio_agent_version" in $facts and $facts["aio_agent_version"] =~ /^6/ {
+    $extra_dirs = [
+      "/etc/puppetlabs/mcollective",
+      "/opt/puppetlabs/mcollective",
+      "/opt/puppetlabs/mcollective/mcollective"
+    ]
+  } else {
+    $extra_dirs = []
+  }
+
   $needed_dirs = [
     "${mcollective::configdir}/plugin.d",
     "${mcollective::configdir}/policies",
     $mcollective::libdir,
     "${mcollective::libdir}/mcollective",
-    $libdirs
-  ]
+  ] + $libdirs + $extra_dirs
 
   if $mcollective::purge {
     $purge_options = {
