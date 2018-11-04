2aade21c689900ea6b07222ed1d05e84bbbebfc2
R.I.Pienaar <rip@devco.net>
2018-11-04 12:59:42 +0100

    (#202) on AIO skip managing the service

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,4 +1,8 @@
 class mcollective::service {
+  if "aio_agent_version" in $facts and $facts["aio_agent_version"] =~ /^6/ {
+    return()
+  }
+
   service{$mcollective::service_name:
     ensure => $mcollective::service_ensure,
     enable => $mcollective::service_enable
